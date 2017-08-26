7c019faedbaaf07aaa209c7663b1e4a5d79413b5
che27235304 <27235304@qq.com>
2017-08-26 15:27:31 +0800

    che

--- PandaChanner/app/src/main/java/com/jiyun/qcloud/dashixummoban/ui/China/China.java
+++ PandaChanner/app/src/main/java/com/jiyun/qcloud/dashixummoban/ui/China/China.java
@@ -1,16 +1,26 @@
 package com.jiyun.qcloud.dashixummoban.ui.China;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.jiyun.qcloud.dashixummoban.R;
 import com.jiyun.qcloud.dashixummoban.base.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by Administrator on 2017/8/23 0023.
  */
 
 public class China extends BaseFragment {
+
+
     @Override
     protected int getLayoutRes() {
         return R.layout.chian;
@@ -30,4 +40,5 @@ public class China extends BaseFragment {
     public void setBundle(Bundle bundle) {
 
     }
+
 }
